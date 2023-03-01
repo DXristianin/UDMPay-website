579b8325bfec01d5985852a85df272221eaee946
DXristianin <75903988+DXristianin@users.noreply.github.com>
2023-03-01 15:27:27 +0300

    Payment mage update

--- js/main.js
+++ js/main.js
@@ -137,3 +137,13 @@ function documentSelect(select, e) {
 function closeInfo(){
   $('.notice').hide();
 }
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
